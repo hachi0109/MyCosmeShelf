22b42ab6949292e31ac80c93d9ad43f58815ef85
hachi0109 <honeywisteria0109@gmail.com>
2026-01-07 12:12:42 +0900

    使用期限アラート実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
     @user = current_user
     @theme = @user.theme_color
     all_cosmetics = @user.cosmetics
-    @expired_count = all_cosmetics.expired.count
+    # 在庫があり(stock > 0)、かつ開封日(open_date)から1年が経過しているものをカウント
+    @expired_count = all_cosmetics.where("stock > 0 AND open_date <= ?", Date.today - 1.year).count
     @stock_summary = all_cosmetics.group_by(&:genre).transform_values(&:count)
 
     @recent_cosmetics = all_cosmetics.order(created_at: :desc).limit(2)
