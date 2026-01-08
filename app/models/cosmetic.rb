bcc7b8e281d054fe94e1a6dcc419d98576f38435
hachi0109 <honeywisteria0109@gmail.com>
2026-01-08 12:05:42 +0900

    バリデーション修正

--- app/models/cosmetic.rb
+++ app/models/cosmetic.rb
@@ -8,7 +8,7 @@ class Cosmetic < ApplicationRecord
   has_one_attached :image
   has_many :comments, dependent: :destroy
 
-  validates :name, :genre_id, :stock, presence: true
+  validates :name, :genre_id, :stock, :usage_count_id, presence: true
   # 「開封日(opened_at)が1年以上前」のデータを取得する定義
   scope :expired, -> { where("open_date < ?", 1.year.ago) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
   has_many :comments, through: :cosmetics
 
   validates :theme_color_id, numericality: { other_than: 1 }
+  validates :name, :theme_color_id, presence: true
 end
