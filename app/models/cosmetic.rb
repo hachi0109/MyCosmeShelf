72a5d2d828911ffe173c84cfcff2397e149c4a76
hachi0109 <honeywisteria0109@gmail.com>
2026-01-08 12:50:34 +0900

    テスト用gem導入

--- app/models/cosmetic.rb
+++ app/models/cosmetic.rb
@@ -14,8 +14,23 @@ class Cosmetic < ApplicationRecord
   validates :usage_count_id, presence: true
 
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true, message: 'は0以上の半角整数で入力してください' }
-  validates :image, content_type: { in: %w[image/jpeg image/png], message: 'はJPEGまたはPNG形式にしてください' },
-                    size: { less_than: 5.megabytes, message: 'は5MB未満のファイルを選択してください' }
+  validate :image_content_type
+  validate :image_size
+
   # 「開封日(opened_at)が1年以上前」のデータを取得する定義
   scope :expired, -> { where("open_date < ?", 1.year.ago) }
+
+  private
+
+  def image_content_type
+    if image.attached? && !image.content_type.in?(%w[image/jpeg image/png])
+      errors.add(:image, 'はJPEGまたはPNG形式にしてください')
+    end
+  end
+
+  def image_size
+    if image.attached? && image.blob.byte_size > 5.megabytes
+      errors.add(:image, 'は5MB未満のファイルを選択してください')
+    end
+  end
 end
